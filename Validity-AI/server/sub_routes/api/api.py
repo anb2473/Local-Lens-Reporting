a04d504e89f34604db14482b4b32f78d17eafa18
Austin <austinnabilblass@gmail.com>
2025-06-16 00:43:22 -0400

    Partial Test Coverage

--- Validity-AI/server/sub_routes/api/api.py
+++ Validity-AI/server/sub_routes/api/api.py
@@ -12,6 +12,10 @@ api_router = APIRouter(
 
 @api_router.get("/ta", response_class=JSONResponse)
 async def text_analyzer(req: Request, res: JSONResponse):
+    if not req.__contains__("query") or req.query.params.get("claim") is None:
+        res.status_code = 400
+        res.body = {"error": "Missing 'claim' parameter in query string."}
+        return  
     claim = req.query.params.get("claim")
     claims = re.split(r'[.;?!]', claim) 
     res.body = analyze_claims_plausibility(claims)
@@ -19,6 +23,10 @@ async def text_analyzer(req: Request, res: JSONResponse):
 
 @api_router.get("/tea", response_class=JSONResponse)
 async def text_equivilence_analyzer(req: Request, res: JSONResponse):
+    if not req.__contains__("query") or req.query.params.get("claim") is None:
+        res.status_code = 400
+        res.body = {"error": "Missing 'claim' parameter in query string."}
+        return  
     claim = req.query.params.get("claim")
     claims = re.split(r'[.;?!]', claim) 
     res.body = check_text_meaning_similarity(claims)
