d1a621ce4d9c43a40185385bac7a57dc1f397adb
Austin <austinnabilblass@gmail.com>
2025-07-20 22:27:58 -0400

    REST Coverage

---
+++ Validity-AI/testing/stress_test.py
@@ -0,0 +1,10 @@
+from locust import HttpUser, task, between
+
+# This is a system to stress test the AI service
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 3) # 1 to 3 seconds
+
+    @task
+    def test_home_page(self):
+        self.client.get("/")
